928499854bc9e357caa5e26b860e2c366d97a7e6
g-na <32394598+ginaphil@users.noreply.github.com>
2022-04-18 01:54:14 +0530

    structure for scrolly

---
+++ src/script/app.js
@@ -0,0 +1,6 @@
+
+import enterViewScrolly from '../scrolly/enterViewScrolly';
+import * as bootstrap from 'bootstrap';
+console.log('Hello from the console world!');
+
+enterViewScrolly("#map-scrolly");
